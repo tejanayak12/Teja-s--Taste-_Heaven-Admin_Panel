c855b742663b6908733895753862e87f09dfe57c
Teja Nayak <prasadnaik1092@gmail.com>
2024-07-28 18:14:21 +0530

    orders page delivery Status Using Api Completed

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -21,6 +21,17 @@ const Orders = ({ url }) => {
         }
     }
 
+    const statusHandler = async (event, orderId) => {
+        const response = await axios.post(url + "/api/order/status", {
+            orderId,
+            status: event.target.value
+        })
+        if (response.data.success) {
+            await fetchAllOrders();
+        }
+    }
+
+
     useEffect(() => {
         fetchAllOrders();
     }, [])
@@ -56,7 +67,7 @@ const Orders = ({ url }) => {
                         </div>
                         <p>Items : {order.items.length}</p>
                         <p>₹{order.amount}</p>
-                        <select>
+                        <select onChange={(event) => statusHandler(event, order._id)} value={order.status}>
                             <option value="Food Processing">Food Processing</option>
                             <option value="Out For Delivery">Out For Delivery</option>
                             <option value="Delivered">Delivered</option>
