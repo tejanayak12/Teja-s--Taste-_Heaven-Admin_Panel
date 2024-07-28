7a588ad9943e445851460ffca5170590a8260ddb
Teja Nayak <prasadnaik1092@gmail.com>
2024-07-28 17:56:15 +0530

    orders list For Admin Panel Completed

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,6 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import logo1 from './logo1.png';
+import parcel from './parcel.png'
 
 export const assets = {
     logo,
@@ -13,7 +14,8 @@ export const assets = {
     profile_image,
     upload_area,
     parcel_icon,
-    logo1
+    logo1,
+    parcel
 }
 
 export const url = 'http://localhost:4000'
